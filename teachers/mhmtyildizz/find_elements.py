70b10d141120530a554025bbd0d02473228cbedf
mhmtyildizz <mhmtyildizz@hotmail.com>
2021-04-26 00:30:46 +0300

    Hata mesajı alıyorum. Find özelliği gelmiyor

--- teachers/mhmtyildizz/find_elements.py
+++ teachers/mhmtyildizz/find_elements.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 import settings
-
 import time
+from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome(settings.driver_path)
 driver.get("https://istanbulakademi.meb.gov.tr/")
 driver.maximize_window()
 
+
+#ekran görüntüsü almak için kullandık
+driver.save_screenshot("./images/ekran-goruntusu.png")
+
+#çıkan pencereyi kapatmak için ekledik
+#button = driver.find_element(By.CLASS_NAME, "btn-warning")
+button = driver.find_element_by_name("btn-warning")
+button.click()
 
 driver.close()
