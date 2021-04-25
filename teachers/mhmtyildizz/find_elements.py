2e3c8e05d6e7dcb564d56979aaf0b3e40f4698e1
mhmtyildizz <mhmtyildizz@hotmail.com>
2021-04-25 23:22:14 +0300

    Update find_elements.py

--- teachers/mhmtyildizz/find_elements.py
+++ teachers/mhmtyildizz/find_elements.py
@@ -4,7 +4,7 @@ import settings
 import time
 
 
-driver = webdriver.Chromo(settings.driver_path)
+driver = webdriver.Chrome(settings.driver_path)
 driver.get("https://istanbulakademi.meb.gov.tr/")
 driver.maximize_window()
 
