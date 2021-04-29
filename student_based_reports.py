c13ead42d673d5819a51b73684f4698550fa3ee8
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2021-04-29 17:02:15 +0300

    EBA'ya giriş işleminin yapılması

--- student_based_reports.py
+++ student_based_reports.py
@@ -16,3 +16,29 @@ ALGORİTMA
     - Performans ortalamasını hesap et
     - Excel' kaydet
 """
+from selenium import webdriver
+import settings
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+
+# tarayıcı nesnesi oluştur
+driver = webdriver.Chrome(settings.driver_path)
+driver.maximize_window()
+# sayfanın yüklemesini çok beklememesi için
+# 10 saniye beklemesini, yoksa hata vermesini belirliyoruz
+driver.set_page_load_timeout(10)
+
+# EBA'ya giriş yap
+try:
+    driver.get("https://giris.eba.gov.tr/EBA_GIRIS/teacher.jsp")
+    driver.find_element_by_xpath("//button[@title='edevlet girişi']").click()
+    driver.find_element_by_css_selector("#tridField").send_keys(settings.tc)
+    driver.find_element_by_id("egpField").send_keys(settings.password)
+    driver.find_element_by_xpath("//input[@name='submitButton']").click()
+except:
+    # tıklama sonrası sayfa 10 saniyede yüklenemedi ise
+    print("Sayfa 10 saniyede yüklenemedi...")
+    driver.refresh()
