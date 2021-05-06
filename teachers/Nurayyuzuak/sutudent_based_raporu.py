0a4f74dc1cb778961417f68aabd958c669065871
Nuray <82217063+Nurayyuzuak@users.noreply.github.com>
2021-05-06 13:05:00 +0300

    ebaya giriş yapıldı

--- teachers/Nurayyuzuak/sutudent_based_raporu.py
+++ teachers/Nurayyuzuak/sutudent_based_raporu.py
@@ -12,6 +12,10 @@ driver.maximize_window()
 
 # EBA'ya giriş yap
 driver.get("https://giris.eba.gov.tr/EBA_GIRIS/teacher.jsp")
+driver.find_element_by_xpath("//button[@title='edevlet girişi']").click()
+driver.find_element_by_xpath("//input[@id='tridField']").send_keys(settings.tc)
+driver.find_element_by_id("egpField").send_keys(settings.password)
+driver.find_element_by_xpath("//input[@name='submitButton']").click()
 time.sleep(2)
 #tarayıcıyı kapat
 driver.close()
