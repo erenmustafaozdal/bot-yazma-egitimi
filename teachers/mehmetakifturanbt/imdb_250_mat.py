5a0cc5ece6c4bf437a575b08b835b67d9a8e0703
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-05-07 21:30:10 +0300

    Update imdb_250_mat.py
    Yeni görünüm

--- teachers/mehmetakifturanbt/imdb_250_mat.py
+++ teachers/mehmetakifturanbt/imdb_250_mat.py
@@ -8,7 +8,7 @@ url="https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&sta
 
 page_count=1
 tum_bilgiler=[]
-while page_count<=50:
+while page_count<=250:
     new_page = url + str(page_count)
     browser.get(new_page)
     elements = browser.find_elements_by_css_selector("div.lister-item-content")
@@ -25,8 +25,11 @@ while page_count<=50:
         oran=element.find_element_by_css_selector("div.inline-block.ratings-imdb-rating").text
         degerler = [len(film_name),len(sure),len(oran)]
         maximum = (max(degerler))
+        bosluk_film = maximum - (len(film_name)) + 1
+        bosluk_sure = maximum - (len(sure)) + 1
+        bosluk_oran = maximum - (len(oran)) + 1
         bilgi=str(page_count)+" | "+film_name+" | Süre : "+sure+" | Oran : "+oran
-        bilgi2=f"{page_count} | {((maximum+7)-(len(str(page_count))))*'~'}\nFilm\t: {film_name}\nSüre\t: {sure}\nOran\t: {oran}"
+        bilgi2=f"| {page_count} |{((maximum+7)-(len(str(page_count))))*'~'}|\n| Film\t: {film_name}{bosluk_film*' '}|\n| Süre\t: {sure}{bosluk_sure*' '}|\n| Oran\t: {oran}{bosluk_oran*' '}|"
         print(len(bilgi)*"~")
         print(bilgi)
         tum_bilgiler.append(bilgi2)
@@ -39,7 +42,7 @@ with open("imdb_top_250_mat.txt", "w", encoding="UTF-8") as file:
         file.write(top_250_film_bilgi + "\n")
         bolum = top_250_film_bilgi.split("\n")
         #print(bolum[0])
-        file.write(len(bolum[0])*"~"+"\n\n")
+        file.write("|"+(len(bolum[0])-2)*"~"+"|"+"\n\n")
         page_count += 1
 
 browser.close()
