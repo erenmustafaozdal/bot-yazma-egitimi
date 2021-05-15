b91232a0ac0dc6d48b9c5d2b5dbf1963b0b2978a
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2021-05-15 10:49:36 +0300

    Her çalışmada kullanmak için Browser nesnesi oluşturuldu

---
+++ classes/browser.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+
+
+class Browser:
+    """
+    Tarayıcı nesnesini oluşturan ve ilk ayarlarını yapan tarayıcı (browser) nesnesi
+    """
+
+    def __init__(self, driver_path):
+        """
+        Yapılandırma metodu
+
+        :param driver_path: çalıştırılabilir tarayıcı yolu
+        """
+        self.driver = webdriver.Chrome(driver_path)
+        self.driver.maximize_window()
+        # sayfanın yüklemesini çok beklememesi için
+        # 20 saniye beklemesini, yoksa hata vermesini belirliyoruz
+        # hata verdiğinde bu hatayı yakalayıp
+        # sayfanın yenilenmesini veya giriş işlemini tekrar etmesini sağlayacağız
+        self.driver.set_page_load_timeout(20)
+
+    def get(self):
+        """
+        :return: webdriver tarayıcı nesnesini geri döndürür
+        """
+        return self.driver
