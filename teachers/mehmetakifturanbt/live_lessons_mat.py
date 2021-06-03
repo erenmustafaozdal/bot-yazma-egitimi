634f724ecb23791eeaacaaba23829a3d7c9e3b0e
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-06-03 03:35:21 +0300

    7 hafta Ek

--- teachers/mehmetakifturanbt/live_lessons_mat.py
+++ teachers/mehmetakifturanbt/live_lessons_mat.py
@@ -29,17 +29,74 @@ ALGORİTMA
 # Todo: Modül ve sınıfları içeri aktar
 from classes.zoom_mat import Zoom
 from classes.telegram_mat import Telegram
+from classes.excel_mat import Excel
+from classes.browser_mat import Browser
+from classes.eba_mat import EBA
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import settings
+import time
+from datetime import datetime, timedelta
 
+# hata yakaladıktan sonra yine de hata hakkında bize mesaj döndürmesi için bu modülü kullanıyoruz
+import logging
+logger = logging.getLogger()
+
+# dictionary veri tipini satırlar halinde ekrana yazdırmak için bu modülü kullanıyoruz
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+# Canlı ders eklerken işimize yarayacak seçiciler (XPATH vb.)
+live_lessons_menu = "//div[normalize-space()='Canlı Dersler']"
+add_live_lesson_btn = "//button[normalize-space()='Harici Canlı Ders Ekle']"
+live_lesson_title = "//div[contains(text(), 'Ders Başlığı')]/following-sibling::input"
+classroom = "//div[contains(text(), 'Sınıf')]/following-sibling::select/option[contains(text(),'{}')]"
+date_picker = "//div[contains(text(), 'Tarih')]/following-sibling::p//button"
+day_btns = "//table[@class='uib-daypicker']//button[not(@disabled)][normalize-space()='{}']"
+lesson_hour = "//div[contains(text(), 'Saat')]/following-sibling::div/select/option[text()='{}']"
+description = "//div[contains(text(), 'Açıklama')]/following-sibling::input"
+lesson_link = "//div[contains(text(), 'Ders Linki')]/following-sibling::input"
+zoom_app = "//div[contains(text(), 'Uygulama')]/following-sibling::select/option[contains(text(),'Zoom')]"
+lesson_password = "//div[contains(text(), 'Ders Şifresi')]/following-sibling::input"
+lesson_select = "//div[contains(text(), 'Ders *')]/following-sibling::div"
+lesson_name = lesson_select + "//span[@class='ui-select-choices-row-inner'][normalize-space()='{}']"
+group_select = "//span[contains(text(), 'Şube')]/parent::div/following-sibling::div"
+group_name = ".//span[contains(text(),'{}')]"
+list_students = "//div[contains(text(), 'ÖĞRENCİLERİ LİSTELE')]"
+send_live_lesson = "//div[contains(text(), 'CANLI DERSİ GÖNDER')]"
+ok_btn = "//a[normalize-space()='TAMAM']"
+lesson_delete_btn = "//div[@class='body-container']/div[@role='row']//div[contains(normalize-space(), '{}') and contains(normalize-space(), '{}')]//i[@class='vt-icon-delete']"
+delete_yes_btn = "//a[normalize-space()='EVET']"
+next_button = "//a[text()='Sonraki']"
 
 
-users = settings.users
-user = users[1]
+##### EXCEL dosyasından aldığımız dictionary türünde bir ders örneği
+# {   'Auto Recording': 'on',
+#     'Açıklama': '31 Mayıs 2021 Pazartesi günü 09:10 - 09:40 saat aralığında '
+#                 'yapılacak Fen Bilimleri dersi.',
+#     'Canlı Ders Başlığı': 'Fen Bilimleri',
+#     'Canlı Ders Tarihi': datetime.datetime(2021, 5, 31, 0, 0),
+#     'Ders': 'Fen Bilimleri',
+#     'Ders Saat Aralığı': '09:10 - 09:40',
+#     'Durum': None,
+#     'Haftanın Günü': 'Pazartesi',
+#     'Host Video': 'on',
+#     'Join Before Host': 'off',
+#     'Meeting ID': 'new',
+#     'Mute Upon Entry': 'on',
+#     'Participant Video': 'on',
+#     'Password': None,
+#     'Request Permission to Unmute Participants': 'on',
+#     'Sınıf': '3. Sınıf',
+#     'Waiting Room': 'off',
+#     'row': 2,
+#     'Şube/Grup': '3. Sınıf / A Şubesi'}
 
-zoom = Zoom(user['zoom_api_key'], user['zoom_api_secret'])
+# users = settings.users
+# user = users[1]
+#
+# zoom = Zoom(user['zoom_api_key'], user['zoom_api_secret'])
 #print(help(zoom))
 # result = zoom.create_meeting({
 #     "topic": "Deneme Dersi",
@@ -57,9 +114,6 @@ zoom = Zoom(user['zoom_api_key'], user['zoom_api_secret'])
 
 #zoom.delete_meeting("75822733963")
 
-telegram = Telegram(user['telegram_api_id'], user['telegram_api_hash'])
-#chat = telegram.get_chat_ids('Deneme')
-telegram.send_message(user['telegram_chat_id'],"Her şey yolunda merkez")
 #pp.pprint(chat)
 
 # telegram = Telegram(user['telegram_api_id'], user['telegram_api_hash'])
@@ -67,7 +121,6 @@ telegram.send_message(user['telegram_chat_id'],"Her şey yolunda merkez")
 # pp.pprint(chat)
 # telegram.send_message(user['telegram_chat_id'], "Deneme", file='./images/student-based-reports/20210508-53 NİSANUR DAYLAN.png')
 
-# Todo: Canlı ders eklerken işimize yarayacak seçiciler (XPATH vb.)
 
 def get_lesson_message(lesson:'dict'):
     """
@@ -85,77 +138,239 @@ def get_lesson_message(lesson:'dict'):
 ➖➖➖➖➖\n
 [{lesson['join_url']}]({lesson['join_url']})"""
 
+# telegram = Telegram(user['telegram_api_id'], user['telegram_api_hash'])
+# date = datetime(2021, 6, 2, 1, 24)
+#chat = telegram.get_chat_ids('Deneme')
+#telegram.send_message(user['telegram_chat_id'],"Her şey yolunda merkez")
+# for i in range(5):
+#     index = i + 1
+#     message = get_lesson_message({
+#         'Canlı Ders Başlığı' : f'{index}. ders başlığı',
+#         'id' : index,
+#         'join_url' : 'https.google.com',
+#         'password' : 'şifre'
+#     })
+#     message_date = date + timedelta(minutes=i)
+#     telegram.send_message("905445930563", message, date = message_date)
 
-# Todo: Kullanıcıları al. Her kullanıcı için aşağıdaki işlemleri yap. (Kullanıcılar döngüsü)
+# telegram.delete_messages('me','google')
 
-# Todo: Telegram nesnesi oluştur
+# Kullanıcıları al. Her kullanıcı için aşağıdaki işlemleri yap. (Kullanıcılar döngüsü)
+for user in settings.users:
 
-# Todo: Geçmiş ders mesajları sil
+    # Telegram nesnesi oluştur
+    telegram = Telegram(user['telegram_api_id'], user['telegram_api_hash'])
 
-# Todo: Kullanıcı canlı dersler Excel dosyasından verileri al.
+    # Geçmiş ders mesajları sil
+    if telegram.ready():
+        telegram.delete_messages(user['telegram_chat_id'], 'CANLI DERS BAŞLIYOR')
 
-    # Todo: Zoom nesnesi oluştur
+    # Kullanıcı canlı dersler Excel dosyasından verileri al.
+    excel = Excel(user['live_lessons_xl'], data_only=True)
+    lessons = excel.get_datas()
+    del excel
 
-    # Todo: Tarayıcı nesnesi oluştur
+    # Zoom nesnesi oluştur
+    zoom = Zoom(user['zoom_api_key'], user['zoom_api_secret'])
 
-    # Todo: EBA nesnesi oluştur
+    # Tarayıcı nesnesi oluştur
+    browser = Browser(settings.driver_path)
+    driver = browser.get()
 
-    # Todo: EBA'ya giriş yap
+    # EBA nesnesi oluştur
+    eba = EBA(driver)
 
-    # Todo: Canlı dersler sayfasına git
+    # EBA'ya giriş yap
+    eba.login_mebbis(user['tc'], user['password'])
 
-    # Todo: Durumu "hazır" olmayan her ders için aşağıdaki işlemleri yap. (Kullanıcının dersleri döngüsü)
+    # Canlı dersler sayfasına git
+    eba.wait.until(ec.element_to_be_clickable(
+        (By.XPATH, live_lessons_menu)
+    )).click()
+    time.sleep(2)
 
-        # Todo: Dersin başlama tarihini düzelt
+    # Durumu "hazır" olmayan her ders için aşağıdaki işlemleri yap. (Kullanıcının dersleri döngüsü)
+    for index, lesson in enumerate(lessons):
+        if lesson['Durum'] == 'hazır':
+            continue
+
+        # Dersin başlama tarihini düzelt
+        start_hour = datetime.strptime(lesson['Ders Saat Aralığı'].split(" - ")[0], "%H:%M")
         #  Zoom ve Telegram'da kullanmak için dersin tarihini ve saat aralığını birleştir
+        lesson["start_time"] = lesson['Canlı Ders Tarihi'].replace(hour=start_hour.hour, minute=start_hour.minute)
 
-            # Todo: Dersi Zoom'da kaydet (try başlangıcı)
-            #  Zoom'dan gelen id, password ve join_url bilgilerini lessons içindeki ders bilgisine ekle
+        print("-"*50)
+        print(f"Ders: {lesson['Canlı Ders Başlığı']} ({lesson['start_time'].strftime('%d.%m.%Y %H:%M')})")
 
-            # Todo: Ders Zoom'da kaydedildi ise durumuna "zoom" yaz
+        try:
+            pass
+            # Dersi Zoom'da kaydet (try başlangıcı)
+            response_data = zoom.create_meeting({
+                "topic": lesson['Canlı Ders Başlığı'],
+                "start_time": lesson["start_time"].strftime("%Y-%m-%dT%H:%M:%S"),
+                "password": "" if lesson["Password"] is None else lesson["Password"],
+                "agenda": "" if lesson["Açıklama"] is None else lesson["Açıklama"],
+                "settings": {
+                    "use_pmi": True if lesson["Meeting ID"] == "pmi" else False,
+                    "host_video": True if lesson["Host Video"] == "on" else False,
+                    "participant_video": True if lesson["Participant Video"] == "on" else False,
+                    "join_before_host": False if lesson["Join Before Host"] == "off" else True,
+                    "mute_upon_entry": False if lesson["Mute Upon Entry"] == "off" else True,
+                    "auto_recording": "local" if lesson["Auto Recording"] == "on" else "none",
+                    "waiting_room": True if lesson["Waiting Room"] == "on" else False,
+                    "request_permission_to_unmute_participants": True if lesson["Request Permission to Unmute Participants"] == "on" else False
+                }
+            })
+            # Zoom'dan gelen id, password ve join_url bilgilerini lessons içindeki ders bilgisine ekle
+            #pp.pprint(response_data)
+            lessons[index] = {**lesson, **response_data}
 
-            # Todo: Dersi EBA'da kaydet (EBA işlemleri başlagıcı: Burası bir fonksiyon içine alınabilir)
+            # Ders Zoom'da kaydedildi ise durumuna "zoom" yaz
+            lessons[index]["Durum"] = "zoom"
 
-            # Todo: Harici canlı ders ekleme sayfasına git
+            # Dersi EBA'da kaydet (EBA işlemleri başlagıcı: Burası bir fonksiyon içine alınabilir)
 
-            # Todo: Canlı dersin meta verilerini ekle (Ders başlığı ve sınıf)
+            # Harici canlı ders ekleme sayfasına git
+            eba.left_menu_is_loaded()
+            eba.wait.until(ec.element_to_be_clickable((By.XPATH, add_live_lesson_btn))).click()
 
-            # Todo: Canlı dersin tarihini seç
+            # Canlı dersin meta verilerini ekle (Ders başlığı ve sınıf)
+            eba.wait.until(ec.visibility_of_element_located(
+                (By.XPATH, live_lesson_title)
+            )).send_keys(lesson['Canlı Ders Başlığı'])
+            driver.find_element_by_xpath(classroom.format(lesson['Sınıf'])).click()
 
-            # Todo: Canlı dersin zaman aralığını seç
+            # Canlı dersin tarihini seç
+            driver.find_element_by_xpath(date_picker).click()
+            elements = driver.find_elements_by_xpath(day_btns.format(
+                lesson['Canlı Ders Tarihi'].strftime('%d')
+            ))
+            elements[-1].click()
+            time.sleep(2)
 
-            # Todo: Canlı dersin açıklamasını ekle
+            # Canlı dersin zaman aralığını seç
+            eba.wait.until(ec.element_to_be_clickable(
+                (By.XPATH, lesson_hour.format(lesson['Ders Saat Aralığı']))
+            )).click()
 
-            # Todo: Canlı dersin Zoom bilgilerini ekle
+            # Canlı dersin açıklamasını ekle
+            driver.find_element_by_xpath(description).send_keys(lesson['Açıklama'])
 
-            # Todo: Canlı dersi seç
+            # Canlı dersin Zoom bilgilerini ekle
+            driver.find_element_by_xpath(lesson_link).send_keys(lessons[index]['join_url'])
+            driver.find_element_by_xpath(lesson_password).send_keys(lessons[index]['password'])
+            driver.find_element_by_xpath(zoom_app).click()
 
-            # Todo: Canlı dersin yapılacağı şube ve grupları seç
-            #  Birden fazla seçim yapılabilir
-            #  Excel çalışma kitabında virgül (,) ile ayrılarak eklenebilir
+            # Canlı dersi seç
+            eba.wait.until(ec.element_to_be_clickable((By.XPATH, lesson_select))).click()
+            eba.wait.until(ec.element_to_be_clickable(
+                (By.XPATH, lesson_name.format(lesson['Ders']))
+            )).click()
 
-            # Todo: Öğrenci listele tıklanır
+            # Canlı dersin yapılacağı şube ve grupları seç
+            # Birden fazla seçim yapılabilir
+            # Excel çalışma kitabında virgül (,) ile ayrılarak eklenebilir
+            element = eba.wait.until(ec.element_to_be_clickable(
+                (By.XPATH, group_select)
+            ))
+            element.click()
+            time.sleep(1)
+            for group in lesson['Şube/Grup'].split(','):
+                element.find_element_by_xpath(
+                    group_name.format(group.strip())
+                ).click()
 
-            # Todo: Öğrenci listele tıkla
+            # Öğrenci listele tıklanır
+            driver.find_element_by_xpath(list_students).click()
 
-            # Todo: Canlı dersi gönder tıkla
+            # Canlı dersi gönder tıkla
+            eba.wait.until(ec.element_to_be_clickable((By.XPATH, send_live_lesson))).click()
 
-            # Todo: Tamam tıkla
+            # Tamam tıkla
+            eba.wait.until(ec.element_to_be_clickable((By.XPATH, ok_btn))).click()
 
-            # Todo: Ders EBA'da kaydedildi ise durumuna "eba" yaz
+            # Ders EBA'da kaydedildi ise durumuna "eba" yaz
+            lessons[index]['Durum'] = 'eba'
 
-            # Todo: Ders için Telegram'da mesaj planla
+            print("Canlı ders EBA'da tanımlandı.")
 
-            # Todo: Ders için mesaj planlandı ise durumuna "hazır" yaz
+            # Ders için Telegram'da mesaj planla
+            if telegram.ready():
+                telegram.send_message(
+                    user['telegram_chat_id'],
+                    get_lesson_message(lessons[index]),
+                    lesson["start_time"]
+                )
 
-        # Todo: Herhangi bir ders kaydı sırasında hata olursa durumunu kontrol et ve aşağıdaki işlemlerden birini yap.
+            # Ders için mesaj planlandı ise durumuna "hazır" yaz
+            lessons[index]['Durum'] = 'hazır'
 
-            # Todo: "Durum = zoom" ise; Zoom'daki dersi sil
 
-            # Todo: "Durum = eba" ise; Zoom'daki ve EBA'daki dersi ve Telegram mesajını sil
+        # Herhangi bir ders kaydı sırasında hata olursa durumunu kontrol et ve aşağıdaki işlemlerden birini yap.
+        except Exception as error:
+            logger.exception(error)
 
-            # Todo: Hata sonrası Excel dosyasın bitenleri yaz
-            #  "Durum" sutünuna her ders için durumu yaz
-            #  Bir sonraki çalıştırmada durumu "hazır" olanlar dışındaki dersler oluşturulacak
+            # "Durum = zoom" ise; Zoom'daki dersi sil
+            if lessons[index]['Durum'] == 'zoom':
+                # zoom dersi silinir
+                zoom.delete_meeting(lessons[index]['id'])
 
-    # Todo: Herhangi bir hata olmadı ve ders kayıtları bittiyse tüm derslerin durumunu temizle
+            # "Durum = eba" ise; Zoom'daki ve EBA'daki dersi ve Telegram mesajını sil
+            elif lessons[index]['Durum'] == 'eba':
+                # zoom dersi silinir
+                zoom.delete_meeting(lessons[index]['id'])
+
+                # EBA'daki ders silinir
+                while True:
+                    # canlı dersler tablosunun yüklenmesi beklenir
+                    eba.table_is_loaded()
+                    try:
+                        # silme tuşunu seç ve dersi sil
+                        element = driver.find_element_by_xpath(
+                            lesson_delete_btn.format(
+                                lesson['start_time'].strftime('%d.%m.%Y'),
+                                lesson['Ders Saat Aralığı']
+                            )
+                        )
+                        ActionChains(driver).click(element).perform()
+                        time.sleep(1)
+                        eba.wait.until(ec.element_to_be_clickable(
+                            (By.XPATH, delete_yes_btn)
+                        )).click()
+                        # silmek için EVET'e bastıktan sonra işlemin tamamlanması için ne olur ne olmaz bir miktar bekleme yapalım
+                        time.sleep(2)
+                        print(f"{lesson['start_time'].strftime('%d.%m.%Y')} {lesson['Ders Saat Aralığı']} tarihli canlı ders EBA'da silindi")
+                        break
+                    # eğer bu sayfada yoksa sonraki sayfaya geçilir
+                    except:
+                        driver.find_element_by_xpath(next_button).click()
+
+                # Telegram'da zamanlanmış mesajı sil
+                if telegram.ready():
+                    telegram.delete_scheduled_messages(user['telegram_chat_id'], lessons[index]['join_url'])
+                    print(f"{lesson['start_time'].strftime('%d.%m.%Y')} {lesson['Ders Saat Aralığı']} tarihli canlı dersin mesajı Telegram'da silindi")
+
+            # Sonraki çalışmada Durumu düzelt
+            lessons[index]['Durum'] = None
+
+            # Hata sonrası Excel dosyasına bitenleri yaz
+            # "Durum" sütununa her ders için durumu yaz
+            # Bir sonraki çalıştırmada durumu "hazır" olanlar dışındaki dersler oluşturulacak
+            excel = Excel(user['live_lessons_xl'])
+            data = [ders['Durum'] for ders in lessons]
+            excel.update_range(excel.headers['Durum']['column'], data, True)
+            excel.save()
+            del excel
+
+            raise
+
+    # alt bilgilerden ayırmak için boşluk yazdır
+    print("-"*50)
+    print("")
+
+    # Herhangi bir hata olmadı ve ders kayıtları bittiyse tüm derslerin durumunu temizle
+    excel = Excel(user['live_lessons_xl'])
+    data = [None for lesson in lessons]
+    excel.update_range(excel.headers['Durum']['column'], data, True)
+    excel.save()
+    del excel
